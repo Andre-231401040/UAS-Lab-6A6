7443c06b6ee46689d09c53a4c9b7114374d14772
EndrianoKitswanto-231401073 <151598728+EndrianoKitswanto-231401073@users.noreply.github.com>
2023-12-07 12:33:48 +0700

    Add files via upload

--- tugasuas.pas
+++ tugasuas.pas
@@ -2,9 +2,13 @@ program gameDadu;
 
 uses crt;
 
+label awal, bertanya;
+
 var
-i, j, jmlhPemain, skorTarget : integer;
-skorPemain, selisihSkor : array of integer;
+jawaban : char;
+ulang : boolean;
+i, jumlahPemainTieBreaker, jmlhPemain, skorTarget : integer;
+skorPemain, selisihSkor, skorTieBreak, selisihSkorPesertaTieBreak : array of integer;
 namaPemain, pemainTieBreaker : array of string;
 
 //function acak dadu
@@ -21,12 +25,12 @@ i, angkaDadu : integer;
 begin
     for i := 0 to jmlhPemain - 1 do
         begin
-            writeln('Pemain ke-',i+1);
+            writeln('Pemain ke-',i+1,' atau ',namaPemain[i]);
             angkaDadu := lemparDadu;
             skorPemain[i] += angkaDadu;
             writeln('Dadu anda bernilai ',angkaDadu);
             writeln('Skor anda saat ini: ',skorPemain[i]);
-            readkey;
+            readln;
             writeln;
         end;
 end;
@@ -92,11 +96,74 @@ begin
         end;
 end;
 
-//prosedur cek ada atau tidaknya tie breaker
+//prosedur cek ada tie breaker atau tidak
+procedure cekTieBreak;
+begin
+    while(i <= jmlhPemain - 1) do
+        begin
+            if(selisihSkor[i] = selisihSkor[i+1]) then
+                begin
+                    pemainTieBreaker[jumlahPemainTieBreaker] := namaPemain[i];
+                    selisihSkorPesertaTieBreak[jumlahPemainTieBreaker] := selisihSkor[i];
+                    jumlahPemainTieBreaker += 1;
+                    i += 1; 
+                end
+            else if(selisihSkor[i] = selisihSkor[i-1]) then
+                begin
+                    pemainTieBreaker[jumlahPemainTieBreaker] := namaPemain[i];
+                    selisihSkorPesertaTieBreak[jumlahPemainTieBreaker] := selisihSkor[i];
+                    jumlahPemainTieBreaker += 1;
+                    i += 1;
+                end
+            else
+                begin
+                    i += 1;
+                end;
+        end;
+end;
 
 //prosedur game tie breaker
+procedure tieBreak(jumlahPemainTieBreaker : integer);
+var
+i, j, sameScoreCounter : integer;
+begin
+    clrscr;
+    ulang := true;
+    while(ulang) do
+        begin
+            sameScoreCounter := 0;
+            for i := 0 to jumlahPemainTieBreaker - 1 do
+                begin
+                    writeln('Pemain ke-',i+1,' atau ',pemainTieBreaker[i]);
+                    skorTieBreak[i] := lemparDadu;
+                    writeln('Dadu anda bernilai ',skorTieBreak[i]);
+                    readln;
+                end;
+
+            for i := 0 to jumlahPemainTieBreaker - 2 do
+                begin
+                    for j := i+1 to jumlahPemainTieBreaker - 1 do
+                        begin
+                            if(skorTieBreak[i] = skorTieBreak[j]) then
+                                begin
+                                    if(selisihSkorPesertaTieBreak[i] = selisihSkorPesertaTieBreak[j]) then
+                                        begin
+                                            sameScoreCounter += 1;
+                                        end;
+                                end;
+                        end;
+                end;
+
+            //cek ada skor yang sama atau engga
+            if(sameScoreCounter = 0) then
+                begin
+                    ulang := false;
+                end;
+        end;
+end;
 
 begin
+    awal:
     writeln('===========================================');
     writeln('Selamat Datang di Game Dadu Kelompok 6A6!!!');
     writeln('===========================================');
@@ -110,6 +177,8 @@ begin
     setlength(namaPemain, jmlhPemain);
     setlength(skorPemain, jmlhPemain);
     setlength(selisihSkor, jmlhPemain);
+    setlength(pemainTieBreaker, jmlhPemain);
+    setlength(selisihSkorPesertaTieBreak, jmlhPemain);
 
     //tambah nama pemain
     writeln;
@@ -125,6 +194,8 @@ begin
     i := 1;
     while(i <= 3) do
         begin
+            clrscr;
+            writeln('Putaran ke-',i);
             bermain(jmlhPemain);
             i += 1;
         end;
@@ -145,24 +216,68 @@ begin
     gotoXY(30,3);writeln('|');
     gotoXY(32,3);writeln('Skor');
     gotoXY(37,3);writeln('|');
-    gotoXY(39,3);writeln('SelisihSkor');
+    gotoXY(39,3);writeln('Selisih Skor');
     writeln('===================================================');
     tampilkanPemenang(jmlhPemain);
+    write('Tekan enter untuk lanjut...');
+    readln;
 
     //cek ada tie breaker atau engga
-    for i := 0 to jmlhPemain - 2 do
+    i := 0;
+    jumlahPemainTieBreaker := 0;
+    cekTieBreak;
+
+    //set ukuran array untuk skorTieBreak berdasarkan jumlahPemainTieBreaker
+    setlength(skorTieBreak, jumlahPemainTieBreaker);
+
+    if(jumlahPemainTieBreaker = 0) then
         begin
-            j := 0;
-            if(selisihSkor[i] = selisihSkor[i+1]) then
+            writeln(jumlahPemainTieBreaker);
+            goto bertanya;
+        end
+    else
+        begin
+            clrscr;
+            writeln('==================================');
+            writeln('Selamat datang di game tie breaker');
+            writeln('==================================');
+            writeln('Peserta tie breaker: ');
+            for i := 0 to jumlahPemainTieBreaker - 1 do
                 begin
-                    pemainTieBreaker[j] := namaPemain[i];
-                    pemainTieBreaker[j+1] := namaPemain[i+1];
-                    i += 1; 
-                    j += 2;
-                end
-            else if(selisihSkor[i] = selisihSkor[i-1]) then
-                begin
-                    pemainTieBreaker[j] := namaPemain[i];
+                    writeln(i+1,'. ',pemainTieBreaker[i]);
                 end;
+            write('Klik tombol enter untuk lanjut bermain...');
+            readln;
+            tieBreak(jumlahPemainTieBreaker);
         end;
+
+    //menampilkan pemenang setelah tie break
+    clrscr;
+    writeln('Skor Target : ', skorTarget); 
+    writeln('===================================================');
+    gotoXY(1,3);writeln('Ranking');
+    gotoXY(9,3);writeln('|');
+    gotoXY(11,3);writeln('Nama');
+    gotoXY(30,3);writeln('|');
+    gotoXY(32,3);writeln('Skor');
+    gotoXY(37,3);writeln('|');
+    gotoXY(39,3);writeln('Selisih Skor');
+    gotoXY(52,3);writeln('|');
+    gotoXY(54,3);writeln('Skor Tie Break');
+    writeln('===================================================');
+    tampilkanPemenang(jmlhPemain);
+    write('Tekan enter untuk lanjut...');
+    readln;
+    //bertanya kepada user pada ingin mengulang atau tidak
+    bertanya:
+    write('Apakah ingin lanjut bermain (Y/T): ');
+    readln(jawaban);
+
+    jawaban := upcase(jawaban);
+
+    if(jawaban = 'Y') then
+        begin
+            goto awal;
+        end;
+    writeln('TERIMA KASIH SUDAH MENCOBA GAME KAMI!!!');
 end.
